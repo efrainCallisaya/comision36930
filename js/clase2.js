3c4c05418aad6b7ed59a889e86ca1858d412f285
efrainCallisaya <efra.man.calli.hino@gmail.com>
2022-03-23 09:57:17 -0300

    subimos primer ejercicio de la comision

---
+++ js/clase2.js
@@ -0,0 +1,10 @@
+let anio = prompt("ingresa tu año");
+const anioActual = 2022;
+let edad = anioActual-anio;
+let edadrestante=0;
+if(edad >= 18){
+    alert("usted pasa");
+}else{
+    edadrestante = 18 - edad;
+    alert("te falta " + edadrestante)
+}
