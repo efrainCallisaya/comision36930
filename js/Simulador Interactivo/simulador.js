ee77e0abc317160225b6101af8d7c7e688d40206
efrainCallisaya <efra.man.calli.hino@gmail.com>
2022-04-04 19:22:26 -0300

    mejoras en codigo

--- js/Simulador Interactivo/simulador.js
+++ js/Simulador Interactivo/simulador.js
@@ -1,4 +1,4 @@
-let cuota= 0
+
 
  
 function card (typeCard, total) {
@@ -16,16 +16,16 @@ function card (typeCard, total) {
 function produto (produto) {
     switch(producto) {
         case "gaseosa":
-           precio = 2
+           precio = 100
            break
         case "papas":
-           precio = 4
+           precio = 70
            break
         case "jugo":
-           precio = 1
+           precio = 50
            break
         case "alfajor": 
-           precio = 4
+           precio = 40
            break
         default:
            alert ("no existe el producto");
@@ -40,16 +40,16 @@ function produto (produto) {
 
 let producto = prompt("Ingrese un producto 'gaseosa, papas, jugo, alfajor")
 let total =0
-
+let cuota= 0
 
 while( producto != "SALIR"){
     let precio = produto (produto);
    total = total + precio;
-   alert("precio encarrito " + total)
+   alert("precio en carrito:  " + total)
    producto = prompt("Ingrese un producto 'gaseosa, papas, jugo, alfajor")
  }
 
 let typeCard = prompt("Ingrese tipo de tarjeta(DEBITO o CREDITO)");
 const totalCuota = card (typeCard, total)
-alert("Usted debera pagar: " + totalCuota + "La cantidad de CUOTAS SELECCIONADAS " + cuota + "MESES")
+alert("Usted debera pagar: " + totalCuota + "en " + cuota + " MESES")
 
